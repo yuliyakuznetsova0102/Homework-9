007ae396dadd30201d6d2ae8725698e2cba3574b
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2024-09-01 21:47:29 +0300

    add src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -12,3 +12,10 @@ def mask_account_card(number: str) -> str:
         new_number == get_mask_account(number.split()[-1])
         result = f"{number[:-20]}{new_number}"
     return result
+
+
+def get_new_data(old_data: str) -> str:
+    """Функция принимает на вход строку с датой и возвращает строку с датой
+    в формате 'ДД.ММ.ГГГГ'"""
+    data_slize = old_data[0:10].split("-")
+    return ".".join(data_slize[::-1])
