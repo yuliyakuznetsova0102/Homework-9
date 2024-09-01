b0bc5424fda48bb695003156f4e2878973df6b81
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2024-09-01 21:37:12 +0300

    add widget.py

--- src/widget.py
+++ src/widget.py
@@ -2,10 +2,13 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(number: str) -> str:
+    """Функция получает строки и маскирует счет или карту"""
+    new_number = ""
     if len(number.split()[-1]) == 16:
         new_number == get_mask_card_number(number.split()[-1])
         result = f"{number[:-16]}{new_number}"
-    elif len(number.split()[-1]) == 20:):
+        return result
+    elif len(number.split()[-1]) == 20:
         new_number == get_mask_account(number.split()[-1])
         result = f"{number[:-20]}{new_number}"
     return result
