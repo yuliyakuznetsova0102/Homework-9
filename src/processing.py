da3cf4eae0b99ceffb5f20d7afc80adae48a8c19
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2024-09-05 17:35:54 +0300

    add src/processing

---
+++ src/processing.py
@@ -0,0 +1,11 @@
+def filter_by_state(list_dict: list[dict]) -> list[dict]:
+    """Функция,возвращает новый список словарей, содержащий только те словари, у
+    которых ключ 'state' соответствует указанном значению"""
+    sorted_list = sorted(list_dict, key=lambda x: x["state"], reverse=True)
+    return sorted_list
+
+
+def sort_by_date(list_dict: list[dict]) -> list[dict]:
+    """Функция, возвращает новый список, отсортированный по дате"""
+    sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse=True)
+    return sorted_list
