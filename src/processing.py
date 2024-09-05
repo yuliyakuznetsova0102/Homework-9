fe6a7e80e3df8b29855b2cd779c402c57c5a0089
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2024-09-05 21:41:35 +0300

    homework 10_1 done

--- src/processing.py
+++ src/processing.py
@@ -5,3 +5,9 @@ def filter_by_state(list_of_dict: List[Dict[str, Any]], state: str = "EXECUTED")
     """Функция,возвращает новый список словарей, содержащий только те словари,
     у которых ключ 'state' соответствует указанном значению"""
     return [d for d in list_of_dict if d.get("state") == state]
+
+
+def sort_by_date(date_list: list, reverse_list: bool = True) -> list | bool:
+    """Функция, возвращает новый список, отсортированный по дате"""
+    sorted_list = sorted(date_list, key=lambda date_dict: date_dict.get("date"), reverse=reverse_list)
+    return sorted_list
