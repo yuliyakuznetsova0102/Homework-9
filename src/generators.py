0d4404159e91e6c38ede69f4555bc003300c2484
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2024-09-11 13:54:09 +0300

    Hw 11.1 tests

--- src/generators.py
+++ src/generators.py
@@ -6,25 +6,25 @@ def filter_by_currency(transactions: list, value: str) -> Iterator:
     """Функция возвращает итератор, который поочередно выдает транзакции,
     где валюта операции соответствует заданной"""
     if transactions == []:
-        sys.exit()
+        sys.exit("Нет транзакции")
     for transaction in transactions:
-        if transaction.get("operatorAmount").get("currency").get("code") == value:
+        if transaction.get("operationAmount").get("currency").get("code") == value:
             yield transaction
-        elif transaction.get("operatorAmount").get("currency").get("code") != value:
-            sys.exit()
+        elif transaction.get("operationAmount").get("currency").get("code") != value:
+            sys.exit("В транзакции нет такого кода")
 
 
-def transaction_discriptions(transactions: list) -> Iterator:
-    """Функция возвращает описание каждой операции"""
+def transaction_descriptions(transactions: list) -> Iterator:
+    """Функция возвращает описание операции"""
     if not transactions:
-        sys.exit()
+        sys.exit("Нет транзакции")
     for transaction in transactions:
-        yield transaction.get("discription")
+        yield transaction.get("description")
 
 
 def card_number_generator(start: int, stop: int) -> Iterator:
     """Функция генерирует номера карт в заданном диапазоне"""
     for i in range(start, stop + 1):
         number_zero = "0000000000000000"
-        card_number = number_zero[: -len(str(i))] + str(i)
+        card_number = number_zero[: -len(str(i))] + str(i)  # Генерация номера карты
         yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
