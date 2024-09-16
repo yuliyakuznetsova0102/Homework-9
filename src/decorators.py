27dcd13d644b497a16f6073d362b61ff2af2b0ea
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2024-09-16 21:22:27 +0300

    Hw 11.2 decorators

---
+++ src/decorators.py
@@ -0,0 +1,27 @@
+from typing import Any, Callable
+
+
+def log(filename: Any) -> Callable:
+    """Декоратор, который автоматически логирует начало и конец выполнения функции,
+    а также ее результаты или возникшие ошибки"""
+
+    def my_decorator(func: Callable) -> Callable:
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                result = func(*args, **kwargs)
+                if filename:
+                    with open(filename, "w") as file:
+                        file.write(f"{func.__name__} ok")
+                else:
+                    print(f"{func.__name__} ok")
+                return result
+            except Exception as e:
+                if filename:
+                    with open(filename, "w") as file:
+                        file.write(f"{func.__name__} error: {e.__class__.__name__}. Inputs: {args}, {kwargs}")
+                else:
+                    print(f"{func.__name__} error: {e.__class__.__name__}. Inputs: {args}, {kwargs}")
+
+        return wrapper
+
+    return my_decorator
