aea175c9564affa072772a12a87778768588a6d7
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2024-09-30 14:35:48 +0300

    Hw 12.2

--- src/external_api.py
+++ src/external_api.py
@@ -5,16 +5,19 @@ import requests
 from dotenv import load_dotenv
 
 load_dotenv()
-values = os.getenv("PASSWORD")
+HEADERS = os.getenv("PASSWORD")
 
 
 def currency_conversion(transaction: Any) -> Any:
     """Функция конвертации"""
     amout = transaction["operationAmount"]["amount"]
-    code = transaction["operationAmount"]["currency"]["code"]
-    to = "RUB"
-    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={code}&amount={amout}"
-    payload = {}
-    response = requests.get(url, headers={"apikey": values}, data=payload)
-    result = response.json()
-    return float(result["result"])
+    currency = transaction["operationAmount"]["currency"]["code"]
+    if currency == "RUB":
+        return float(amout)
+    else:
+        response = requests.get(
+            url=f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amout}",
+            headers=HEADERS,
+        )
+        result = response.json()
+        return float(result["result"])
--- src/utils.py
+++ src/utils.py
@@ -2,12 +2,11 @@ import json
 import logging
 from json import JSONDecodeError
 from typing import Any
-from venv import logger
 
 from src.external_api import currency_conversion
 
 utils_logger = logging.getLogger("utils")
-file_handler = logging.FileHandler("../logs/utils.log", mode="w", encoding="utf-8")
+file_handler = logging.FileHandler("logs/utils.log", mode="w", encoding="utf-8")
 file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(massage)s")
 file_handler.setFormatter(file_formatter)
 utils_logger.addHandler(file_handler)
@@ -17,27 +16,27 @@ utils_logger.setLevel(logging.DEBUG)
 def financial_transactions(path: str) -> list:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
     try:
-        logger.info(f"Получение данных из файла {path}")
+        utils_logger.info(f"Получение данных из файла {path}")
         with open(path, encoding="utf-8") as financial_file:
             try:
                 transactions = json.load(financial_file)
             except JSONDecodeError:
-                logger.error(f'Ошибка при чтении json-файла из файла "{path}"')
+                utils_logger.error(f'Ошибка при чтении json-файла из файла "{path}"')
                 return []
         if not isinstance(transactions, list):
             return []
         return transactions
     except FileNotFoundError:
-        logger.error(f'Ошибка:файл "{path}" не найден')
+        utils_logger.error(f'Ошибка:файл "{path}" не найден')
         return []
 
 
-def transaction_amount(trans: dict, currency: str = "RUB") -> Any:
+def transaction_amount(transaction: dict, currency: str = "RUB") -> Any:
     """Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
-    if trans["operationAmount"]["currency"]["code"] == currency:
-        amount = trans["operationAmount"]["amount"]
-        logger.info("Код валюты в транзакции RUB")
+    if transaction["operationAmount"]["currency"]["code"] == currency:
+        amount = transaction["operationAmount"]["amount"]
+        utils_logger.info("Код валюты в транзакции RUB")
     else:
-        amount = currency_conversion(trans)
-        logger.info("Код валюты транзакции не RUB, произведена конвертация")
+        amount = currency_conversion(transaction)
+        utils_logger.info("Код валюты транзакции не RUB, произведена конвертация")
     return amount
